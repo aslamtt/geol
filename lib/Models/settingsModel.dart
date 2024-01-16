1b05d92820cf0e7214c21047e5642bb75346d096
Aslam <aslamtt553@gmail.com>
2024-01-16 15:42:34 +0530

    model

--- lib/Models/settingsModel.dart
+++ lib/Models/settingsModel.dart
@@ -1,11 +1,13 @@
 class SettingModel{
   int? productId;
   int? vendorid;
+  List<String>? skuSet;
 
 //<editor-fold desc="Data Methods">
   SettingModel({
     this.productId,
     this.vendorid,
+    this.skuSet,
   });
 
   @override
@@ -14,26 +16,30 @@ class SettingModel{
       (other is SettingModel &&
           runtimeType == other.runtimeType &&
           productId == other.productId &&
-          vendorid == other.vendorid);
+          vendorid == other.vendorid &&
+          skuSet == other.skuSet);
 
   @override
-  int get hashCode => productId.hashCode ^ vendorid.hashCode;
+  int get hashCode => productId.hashCode ^ vendorid.hashCode ^ skuSet.hashCode;
 
   @override
   String toString() {
     return 'SettingModel{' +
         ' productId: $productId,' +
         ' vendorid: $vendorid,' +
+        ' skuSet: $skuSet,' +
         '}';
   }
 
   SettingModel copyWith({
     int? productId,
     int? vendorid,
+    List<String>? skuSet,
   }) {
     return SettingModel(
       productId: productId ?? this.productId,
       vendorid: vendorid ?? this.vendorid,
+      skuSet: skuSet ?? this.skuSet,
     );
   }
 
@@ -41,6 +47,7 @@ class SettingModel{
     return {
       'productId': this.productId,
       'vendorid': this.vendorid,
+      'skuSet': this.skuSet,
     };
   }
 
@@ -48,8 +55,8 @@ class SettingModel{
     return SettingModel(
       productId: map['productId'] as int,
       vendorid: map['vendorid'] as int,
+      skuSet:List<String>.from(map['skuSet'] ),
     );
   }
-
 //</editor-fold>
 }
